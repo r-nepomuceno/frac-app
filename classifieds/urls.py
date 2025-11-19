b016c64453c1b43798831e0d1a4a7e19b2d08d53
r-nepomuceno <robby.nepomuceno@yale.edu>
2025-11-19 17:44:49 +0000

    Fix classifieds URLs (executives only)

--- classifieds/urls.py
+++ classifieds/urls.py
@@ -2,9 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.exec_list, name="exec_list"),
-    path("<int:pk>/", views.exec_detail, name="exec_detail"),
+    path("", views.exec_list, name="exec_list"),            # /executives/
+    path("<int:pk>/", views.exec_detail, name="exec_detail")  # /executives/1/
 ]
-
-path("jobs/", views.job_list, name="job_list"),
-path("jobs/new/", views.post_job, name="post_job"),
